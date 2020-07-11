a3d9ffec990ea629d577f809c2eb95987f8a78b2
seanjkr <seankoppreddy@gmail.com>
2020-07-11 16:29:18 -0500

    Continued updating everything to jQuery- set up modal and top button,etc.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var repository= (function() {
   var list= []
 
-  var modalContainer = document.querySelector('#modal-container');
+  var modalContainer = $('#modal-container');
 
   function add(pokemon) {
     list.push(pokemon);
@@ -25,35 +25,81 @@ var repository= (function() {
     return list;
   }
 
-  function showModal() {
+  function loadDetails(item) {
+    var url = item.detailsUrl;
+    return $.ajax(url, {dataType: 'json'}).then(function (details) {
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+      }).catch(function (e) {
+        console.error(e);
+      });
+    }
 
-    modalContainer.innerHTML = '';
-
-    var modal = document.createElement('div');
-    modal.classList.add('modal');
-
-    modalContainer.appendChild(modal);
-
-    modalContainer.classList.add('is-visible');
+  function closeModal() {
+    modalContainer.removeClass('is-visible');
   }
 
+  function showModal(item) {
+    $('#modal-container').addClass('is-visible');
+    $('.modal-close').on('click', function (event) {
+      closeModal();
+    });
+    $('.modal').append( '<h2> ' + item.name + ' </h2> <p> Height(dm): ' + item.height + ' </p> <img src= " ' + item.imageUrl + '" class="modal-image" >');
+  }
+
+  $('#modal-container').on('click', function(event) {
+    closeModal();
+  })
+
+  $(document).keydown(function(event) {
+    if(event.key === 'Escape') {
+      closeModal();
+    }
+  });
 
   function addListItem(pokemon) {
-    $('.pokemon-list').append('<li> <button class="pokemon-button">' + pokemon.name + '</button> </li>');
+    $('.pokemon-list').append('<li> <button class="pokemon-button">' + pokemon.name +'</button> </li>');
     $('.pokemon-button').on('click', function (event) {
-      showModal();
+      showDetails(pokemon);
     });
   }
+
+  function showDetails(item) {
+   loadDetails(item).then(function() {
+     showModal(item);
+   });
+ }
 
   return {
     add: add,
     loadList: loadList,
     getAll: getAll,
+    loadDetails: loadDetails,
+    closeModal: closeModal,
     showModal: showModal,
-    addListItem: addListItem
+    addListItem: addListItem,
+    showDetails: showDetails
   }
 })();
 
 repository.loadList().then(function() {
-  repository.getAll().forEach(repository.addListItem);
+  repository.getAll().forEach(repository.addListItem)
 });
+
+
+var backToTop = (function() {
+
+  var scrollToTopButton = $('#js-top');
+
+  var scrollToTop = () => {
+      var c = document.documentElement.scrollTop || document.body.scrollTop;
+      if (c > 0) {
+        window.scrollTo(0, c - c / 10);
+      }
+    };
+
+    scrollToTopButton.onclick = function(e) {
+      e.preventDefault();
+      scrollToTop();
+    }
+})();
